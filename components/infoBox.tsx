077d3e9e6164223590098d44bcb299bef984d28c
ram saloke <ramsaloke117@gmail.com>
2024-12-19 14:06:48 +0530

    added responsivness

--- components/infoBox.tsx
+++ components/infoBox.tsx
@@ -1,3 +1,73 @@
+// import React from 'react';
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Typography from '@mui/material/Typography';
+// import LightModeSharpIcon from '@mui/icons-material/LightModeSharp';
+// import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+// import AcUnitIcon from '@mui/icons-material/AcUnit';
+
+
+// type Data = {
+//   city: string;
+//   temp: number;
+//   maxTemp: number;
+//   minTemp: number;
+//   humidity: number;
+//   feelsLike: number;
+//   description: string;
+// };
+
+// interface InfoBoxProps {
+//   dataInfo: Data;
+// }
+
+// const InfoBox:React.FC <InfoBoxProps> =  ({dataInfo}) => {
+  
+// const RAIN_URL="/weather_images/rain.jpg"
+// const COLD_URL= "/weather_images/winter.jpg"
+// const SUN_URL="/weather_images/summer.jpg"
+
+
+//   return (
+//     <div className="flex items-center justify-center text-center mt-2" >
+//     <div>
+//         <h1 className='text-2xl font-bold mb-2 '>Weather Info of 
+//     <i className='text-violet-600 text-2xl'> {dataInfo.city}</i> </h1>
+//     <Card sx={{ maxWidth: 345 }}>
+//       <CardMedia
+//         sx={{ height: 140 }}
+//         image={dataInfo.humidity>80 ? RAIN_URL : dataInfo.temp > 18 ? SUN_URL : COLD_URL}
+//         title="green iguana"
+//       />
+//       <CardContent>
+//         <Typography gutterBottom variant="h5" component="div">
+//           {dataInfo.city} {dataInfo.humidity>80 ? <ThunderstormIcon/>  : dataInfo.temp > 18 ? <LightModeSharpIcon/> : <AcUnitIcon/> }
+//         </Typography>
+//         <Typography variant="body2" component={"span"}>
+//          <div className=' text-gray-700'>
+//          <p>Temprature : <b>{dataInfo.temp}&deg;C </b></p>
+//          <p>Humidity : <b>{dataInfo.humidity}&deg;C </b></p> 
+//          <p>Min Temperature : <b>{dataInfo.minTemp}&deg;C </b></p>
+//          <p>Max Temperature : <b>{dataInfo.maxTemp}&deg;C </b></p>
+//          <p>Weather can be described as <b>{dataInfo.description}</b> and feels like <b>
+//           {dataInfo.feelsLike}&deg;C </b></p>
+      
+     
+//          </div>
+
+
+//         </Typography>
+//       </CardContent>
+//     </Card>
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default InfoBox
+
+
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -6,7 +76,6 @@ import Typography from '@mui/material/Typography';
 import LightModeSharpIcon from '@mui/icons-material/LightModeSharp';
 import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
-
 
 type Data = {
   city: string;
@@ -22,47 +91,52 @@ interface InfoBoxProps {
   dataInfo: Data;
 }
 
-const InfoBox:React.FC <InfoBoxProps> =  ({dataInfo}) => {
-  
-const RAIN_URL="/weather_images/rain.jpg"
-const COLD_URL= "/weather_images/winter.jpg"
-const SUN_URL="/weather_images/summer.jpg"
-
+const InfoBox: React.FC<InfoBoxProps> = ({ dataInfo }) => {
+  const RAIN_URL = '/weather_images/rain.jpg';
+  const COLD_URL = '/weather_images/winter.jpg';
+  const SUN_URL = '/weather_images/summer.jpg';
 
   return (
-    <div className="flex items-center justify-center text-center mt-2" >
-    <div>
-        <h1 className='text-2xl font-bold mb-2 '>Weather Info of 
-    <i className='text-violet-600 text-2xl'> {dataInfo.city}</i> </h1>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={dataInfo.humidity>80 ? RAIN_URL : dataInfo.temp > 18 ? SUN_URL : COLD_URL}
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {dataInfo.city} {dataInfo.humidity>80 ? <ThunderstormIcon/>  : dataInfo.temp > 18 ? <LightModeSharpIcon/> : <AcUnitIcon/> }
-        </Typography>
-        <Typography variant="body2" component={"span"}>
-         <div className=' text-gray-700'>
-         <p>Temprature : <b>{dataInfo.temp}&deg;C </b></p>
-         <p>Humidity : <b>{dataInfo.humidity}&deg;C </b></p> 
-         <p>Min Temperature : <b>{dataInfo.minTemp}&deg;C </b></p>
-         <p>Max Temperature : <b>{dataInfo.maxTemp}&deg;C </b></p>
-         <p>Weather can be described as <b>{dataInfo.description}</b> and feels like <b>
-          {dataInfo.feelsLike}&deg;C </b></p>
-      
-     
-         </div>
-
-
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className="flex items-center justify-center text-center mt-2">
+      <div className="w-full ">
+        <h1 className="text-2xl font-bold mb-2">
+          Weather Info of <i className="text-violet-600">{dataInfo.city}</i>
+        </h1>
+        <Card className="w-full">
+          <CardMedia
+           sx={{ height: 180 }}
+            image={dataInfo.humidity > 80 ? RAIN_URL : dataInfo.temp > 18 ? SUN_URL : COLD_URL}
+            title="weather image"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" className="flex items-center justify-center">
+              {dataInfo.city}{' '}
+              {dataInfo.humidity > 80 ? <ThunderstormIcon className="ml-2" /> : dataInfo.temp > 18 ? <LightModeSharpIcon className="ml-2" /> : <AcUnitIcon className="ml-2" />}
+            </Typography>
+            <Typography variant="body2" component="span">
+              <div className="text-gray-700 space-y-2">
+                <p>
+                  Temperature: <b>{dataInfo.temp}&deg;C</b>
+                </p>
+                <p>
+                  Humidity: <b>{dataInfo.humidity}%</b>
+                </p>
+                <p>
+                  Min Temperature: <b>{dataInfo.minTemp}&deg;C</b>
+                </p>
+                <p>
+                  Max Temperature: <b>{dataInfo.maxTemp}&deg;C</b>
+                </p>
+                <p>
+                  Weather can be described as <b>{dataInfo.description}</b> and feels like <b>{dataInfo.feelsLike}&deg;C</b>
+                </p>
+              </div>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default InfoBox
+export default InfoBox;
